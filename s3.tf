856200ef3954ebd9fba975e56a5ae2e636d6fbaa
Carlo Mencarelli <me@carlo.cloud>
2023-03-14 21:56:35 -0700

    deprecated arg

--- s3.tf
+++ s3.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "transfer_bucket" {
   bucket = "${local.account_id}-transfer-bucket"
+}
+
+resource "aws_s3_bucket_acl" "transfer_bucket_bucket_acl" {
+  bucket = aws_s3_bucket.transfer_bucket.id
   acl    = "private"
 }
